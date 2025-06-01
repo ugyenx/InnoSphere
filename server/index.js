41ca2d4242272a11c98af19775f34a8e2ad6345d
Govinda Upadhya <upadhyagovinda52@gmail.com>
2025-06-01 16:07:44 +0530

    done

--- server/index.js
+++ server/index.js
@@ -7,9 +7,7 @@ import { paymentRoutes } from "./Routes/payments.js";
 import cookieParser from "cookie-parser";
 const app = express();
 
-mongoose.connect(
-  "mongodb+srv://upadhyagovinda01:K0egzI9gHC3Zh3Vw@cluster0.s0p1pnn.mongodb.net/"
-);
+mongoose.connect("");
 app.use(express.json());
 
 app.use(
